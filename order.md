eb94df12b5bff27cf8815563f106a4f99fc88e9d
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-05-30 13:15:51 -1000

    Setup basic webpack settings

---
+++ order.md
@@ -0,0 +1,4 @@
+npm init -y
+npm install webpack webpack-cli --save-dev
+npm i -D sass style-loader css-loader sass-loader
+npm i -D html-webpack-plugin
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { Script } = require("vm");
+const { type } = require("os");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather App",
+      template: "src/template.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
